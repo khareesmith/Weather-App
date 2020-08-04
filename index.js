d85747a1854bd7270bddc499e74efba383808a95
khareesmith <kharee.smith@yahoo.com>
2020-08-04 14:55:30 -0400

    set-up basic weather api call to OpenWeatherApi

--- index.js
+++ index.js
@@ -6,6 +6,23 @@ $("#weather-btn").click(function (e) {
   let city = $("#city").val();
   console.log(city);
 
-  let queryURL = "api.openweathermap.org/data/2.5/forecast?q=" + city + "&units=imperial&appid=" + APIKey;
-  console.log(queryURL);
+  let queryURL = "https://api.openweathermap.org/data/2.5/forecast?q=" + city + "&units=imperial&appid=" + APIKey;
+
+  $.ajax({
+    url: queryURL,
+    method: "GET"
+  })
+    // We store all of the retrieved data inside of an object called "response"
+    .then(function(response) {
+        console.log(queryURL);
+        console.log(response);
+  
+      // Transfer Content to HTML using jQuery
+      // console.log your data response
+
+      
+  
+    });
+  
+
 });
